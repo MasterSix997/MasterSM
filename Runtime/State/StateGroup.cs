6e3d0c30050b84f8f5c54f0e87b116ace5d49a4b
MasterSix <zanohelp132@gmail.com>
2025-01-31 12:42:12 -0300

    Add StateGroup.cs

---
+++ Runtime/State/StateGroup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MasterSM
+{
+    [Serializable]
+    public class StateGroup<TStateId, TStateMachine> where TStateMachine : IStateMachine
+    {
+        public int basePriority;
+        [SerializeReference] private List<IState<TStateId, TStateMachine>> states = new();
+        [SerializeReference] private List<TStateId> stateIds = new();
+        
+        public IReadOnlyList<IState<TStateId, TStateMachine>> States => states;
+    
+        public StateGroup(params (TStateId id, IState<TStateId, TStateMachine> state)[] states)
+        {
+            foreach (var state in states)
+                AddState(state.id, state.state);
+        }
+        
+        public StateGroup(params IState<TStateId, TStateMachine>[] states)
+        {
+            if (typeof(TStateId) is not Type)
+                return;
+            
+            foreach (var state in states)
+                AddState((TStateId)(object)state.GetType(), state);
+        }
+
+        public void AddState(TStateId id, IState<TStateId, TStateMachine> state)
+        {
+            if (!states.Contains(state) && !stateIds.Contains(id))
+            {
+                states.Add(state);
+                stateIds.Add(id);
+            }
+        }
+    
+        public void RemoveState(IState<TStateId, TStateMachine> state)
+        {
+            states.Remove(state);
+        }
+
+        public IEnumerable<(TStateId id, IState<TStateId, TStateMachine> state)> GetStatesByLowestPriority()
+        {
+            // return states.AsEnumerable().Reverse();
+            for (var i = states.Count - 1; i >= 0; i--)
+            {
+                yield return (stateIds[i], states[i]);
+            }
+        }
+    }
+}
